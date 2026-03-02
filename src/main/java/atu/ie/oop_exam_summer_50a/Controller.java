2f7c28746cae65d2c74c7197804ab6d491de71b6
https://github.com/irfanefedonmez/Oop_exam_summer_50.git <g00472272@atu.ie>
2026-03-02 12:04:58 +0000

    updatd controller and little bit service

--- src/main/java/atu/ie/oop_exam_summer_50a/Controller.java
+++ src/main/java/atu/ie/oop_exam_summer_50a/Controller.java
@@ -2,10 +2,7 @@ package atu.ie.oop_exam_summer_50a;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookings")
@@ -13,11 +10,12 @@ public class Controller {
     private bookingService bookingService;
     public Controller(bookingService bookingService) {
         this.bookingService = bookingService;
+
     }
     @PostMapping
-
-
-    public ResponseEntity<String> bookingService(@Valid @RequestBody String studentEmail) {
-
+    public ResponseEntity <bookingService>bookingService(@Valid @RequestBody bookingService bookingService) {
+        return ResponseEntity.ok(bookingService);
     }
 }
+
+
--- src/main/java/atu/ie/oop_exam_summer_50a/bookingService.java
+++ src/main/java/atu/ie/oop_exam_summer_50a/bookingService.java
@@ -1,6 +1,15 @@
 package atu.ie.oop_exam_summer_50a;
 
 
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
+@Service
 public class bookingService {
+    public bookingService(@Valid @RequestBody bookingService bookingService) {
+
+    }
 }
