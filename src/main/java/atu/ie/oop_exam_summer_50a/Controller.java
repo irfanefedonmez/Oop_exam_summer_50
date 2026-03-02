d001a2183ed81e569d84ddaab610920189a70337
https://github.com/irfanefedonmez/Oop_exam_summer_50.git <g00472272@atu.ie>
2026-03-02 12:26:49 +0000

    added a service called add and did some changes in controller

--- src/main/java/atu/ie/oop_exam_summer_50a/Controller.java
+++ src/main/java/atu/ie/oop_exam_summer_50a/Controller.java
@@ -1,5 +1,5 @@
 package atu.ie.oop_exam_summer_50a;
-
+import atu.ie.oop_exam_summer_50a.bookingService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +15,12 @@ public class Controller {
     @PostMapping
     public ResponseEntity <bookingService>bookingService(@Valid @RequestBody bookingService bookingService) {
         return ResponseEntity.ok(bookingService);
+    }
+    @GetMapping("/no1"){
+        public ResponseEntity<?>add(@RequestParam int no1   , @RequestParam int no2){
+            int result = service.add(no1,no2);
+        }
+
     }
 }
 
--- src/main/java/atu/ie/oop_exam_summer_50a/bookingService.java
+++ src/main/java/atu/ie/oop_exam_summer_50a/bookingService.java
@@ -2,6 +2,7 @@ package atu.ie.oop_exam_summer_50a;
 
 
 import jakarta.validation.Valid;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -9,7 +10,13 @@ import java.util.List;
 
 @Service
 public class bookingService {
-    public bookingService(@Valid @RequestBody bookingService bookingService) {
-
+    public int add(int a, int b) {
+        return a+b;
     }
+
+
+
+
+
+
 }
